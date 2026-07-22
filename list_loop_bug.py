c652defc3999879c0dc7b06528bf9103484cf307
manisha-analytics <venkatamanisha04@gmail.com>
2026-07-22 19:47:28 +0530

    Correct list removal logic in loop
    Fix list modification during iteration issue.

--- list_loop_bug.py
+++ list_loop_bug.py
@@ -3,3 +3,6 @@ for i in num:
     if i <3 :
         num.remove(i)
 print(num)
+
+##in this case index value are taken after removeing the 1 list updated that 2 in 0th indext here 1 on index in 3,
+because i in 1 so i<3 is false.know answer is [2,3,4,5].
