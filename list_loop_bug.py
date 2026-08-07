dfda6784787e0fe871a23c449c88a2bee3313d55
manisha-analytics <venkatamanisha04@gmail.com>
2026-08-07 21:48:27 +0530

    Correct list manipulation and enhance comments
    Fix logic in list manipulation and update comments for clarity.

--- list_loop_bug.py
+++ list_loop_bug.py
@@ -6,3 +6,13 @@ print(num)
 
 ##in this case index value are taken after removeing the 1 list updated that 2 in 0th indext here 1 on index in 3,
 because i in 1 so i<3 is false.know answer is [2,3,4,5].
+
+names=['ram','krisha','arjun']
+names.insert(1,names.pop())
+print(names)
+
+in this case single line run two ways  pop() removes the laste emlemt and retrive it than inset place the value in 
+1 index of names list so out put is [r'ram','arjun','krishna'
+
+                                     
+                                     
